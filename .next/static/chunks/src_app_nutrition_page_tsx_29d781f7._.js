bee7c01b88b144a48600c8f6b7fce39b1b0db13e
avan365 <avanthi365@gmail.com>
2025-07-22 14:28:43 +0800

    Add .next/static/chunks/src_app_nutrition_page_tsx_29d781f7._.js 1753165723268

---
+++ .next/static/chunks/src_app_nutrition_page_tsx_29d781f7._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_42265f9e._.js",
+  "static/chunks/node_modules_33d96565._.js"
+],
+    source: "dynamic"
+});
